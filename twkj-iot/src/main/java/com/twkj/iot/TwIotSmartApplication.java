5541f1bcf2730bb9d25a97aa342d6fdb31fecee4
blue <763602352@qq.com>
2019-04-12 16:38:02 +0800

    first commit

---
+++ twkj-iot/src/main/java/com/twkj/iot/TwIotSmartApplication.java
@@ -0,0 +1,13 @@
+package com.twkj.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwIotSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwIotSmartApplication.class, args);
+    }
+
+}
---
+++ twkj-iot/src/main/java/com/twkj/iot/util/DateUtil.java
@@ -0,0 +1,10 @@
+package com.twkj.iot.util;
+
+/**
+ * @ClassName DateUtil
+ * @Author weijunyong
+ * @Date 2019/4/12 16:35
+ * @Version 1.0
+ **/
+public class DateUtil {
+}
